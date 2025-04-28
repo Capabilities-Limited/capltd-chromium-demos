3109166de4054f031d8f050a008e930571a30cbe
Graeme Jenkinson <gcjenkinson@gmail.com>
2025-04-28 11:11:57 +0100

    Ensure setup is idempotent.

--- 20250509-bay-area-demo/nginx/setup.sh
+++ 20250509-bay-area-demo/nginx/setup.sh
@@ -33,7 +33,7 @@ if [ ! -d /usr/local/www/dsbd-demo ]; then
     wget -r -k -p --no-parent https://www.cl.cam.ac.uk/research/security/ctsrd/cheri/
     mv www.cl.cam.ac.uk /usr/local/www/dsbd-demo
 fi
-cp -r exploits-demo /usr/local/www/exploits
+rsync -rv exploits-demo/ /usr/local/www/exploits/
 
 sysrc nginx_enable=YES
 service nginx start
