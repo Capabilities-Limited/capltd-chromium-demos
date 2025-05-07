736686173d58a9c4603c2d319abcabef3976a9d9
Brooks Davis <brooks@one-eyed-alien.net>
2025-05-07 13:37:16 -0700

    nginx: install rsync if required

--- 20250509-bay-area-demo/nginx/setup.sh
+++ 20250509-bay-area-demo/nginx/setup.sh
@@ -33,6 +33,12 @@ if [ ! -d /usr/local/www/dsbd-demo ]; then
     wget -r -k -p --no-parent https://www.cl.cam.ac.uk/research/security/ctsrd/cheri/
     mv www.cl.cam.ac.uk /usr/local/www/dsbd-demo
 fi
+if $(pkg64 check -d rsync); then
+    echo "rsync installed"
+else
+    echo "Installing rsync"
+    pkg64 install -y rsync
+fi
 rsync -rv exploits-demo/ /usr/local/www/exploits/
 
 sysrc nginx_enable=YES
